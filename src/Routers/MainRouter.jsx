80f7ccd73319ea2aae73de1ad5544419a08b989f
anjumnr99 <anjumanowara99@gmail.com>
2023-12-21 15:25:51 +0600

    first commit

---
+++ src/Routers/MainRouter.jsx
@@ -0,0 +1,11 @@
+import { createBrowserRouter } from "react-router-dom";
+
+
+const MainRouter = createBrowserRouter([
+    {
+      path: "/",
+      element: <div>Hello world!</div>,
+    },
+  ]);
+
+export default MainRouter;
