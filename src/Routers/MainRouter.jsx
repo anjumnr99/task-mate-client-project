6e1d7731cb63db6cd91df569bb9dfaecc40e7dc2
anjumnr99 <anjumanowara99@gmail.com>
2023-12-21 17:25:52 +0600

    register page added

--- src/Routers/MainRouter.jsx
+++ src/Routers/MainRouter.jsx
@@ -1,11 +1,30 @@
 import { createBrowserRouter } from "react-router-dom";
+import LandingPage from "../Pages/LandingPage/LandingPage";
+import Register from "../Register/Register";
+import MainLayout from "../Layout/MainLayout";
+import Login from "../Login/Login";
 
 
 const MainRouter = createBrowserRouter([
     {
       path: "/",
-      element: <div>Hello world!</div>,
-    },
+      element: <MainLayout></MainLayout>,
+      children:[
+        {
+            path:'/',
+            element: <LandingPage></LandingPage>
+        },
+        {
+            path:'register',
+            element: <Register></Register>
+        },
+        {
+            path:'login',
+            element: <Login></Login>
+        },
+      ]
+    }
+
   ]);
 
 export default MainRouter;
